e43280735b605c2b6d942352d64a6adffc4c3e31
imlkh <imlkh84@gmail.com>
2023-11-04 17:54:03 +0900

    parse integer

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,14 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
-    } else {
+        // return serde_json::json!(string.to_string());
+    } else if encoded_value.chars().next().unwrap() == 'i' && encoded_value.chars().last().unwrap() == 'e' {
+        let number_string = &encoded_value[1..encoded_value.len() - 1];
+        let number = number_string.parse::<i64>().unwrap();
+        return serde_json::Value::Number(number.into());
+        // return serde_json::json!(number);
+    }
+    else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
 }
